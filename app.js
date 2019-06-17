c22d5f546aca4ef03e15ff52e708b9f00d454c50
Kaiichiro Ota <kigh.ota@gmail.com>
2019-06-17 23:11:54 +0900

    掃除

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 const Timer = require('./timer');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,30 +14,26 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 const timer = new Timer(handleTick);
 timer.setTime(0, 10);
 timer.start();
+
+// Main Endpoint
 app.get('/', (req, res, next) => {
   const { min, sec } = timer.getTime();
   res.render('timer', { min, sec });
 });
 
+// Endpoint for Server-Sent Events
+// Ref. https://qiita.com/akameco/items/c54af5af35ef9b500b54
 let clientId = 0;
 let clients = {};
-
 app.get('/events', (req, res) => {
   req.socket.setTimeout(86400);
   res.writeHead(200, {
-    // text/event-stream を追加
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    Connection: 'keep-alive'
+    'Content-Type': 'text/event-stream'
   });
   res.write('\n');
   (clientId => {
@@ -56,7 +48,6 @@ function handleTick(sec) {
   const msg = sec;
   console.log('Clients: ' + Object.keys(clients) + ' <- ' + msg);
   for (let clientId in clients) {
-    // メッセージの送信 \n\nが必要
     clients[clientId].write('data: ' + msg + '\n\n');
   }
 }
