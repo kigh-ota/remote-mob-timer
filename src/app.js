e5392675212c240d5e4f3fa809626ee4b8c6af16
Kaiichiro Ota <kigh.ota@gmail.com>
2019-06-26 21:42:27 +0900

    move files

--- src/app.js
+++ src/app.js
@@ -8,14 +8,14 @@ const Timer = require('./timer');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 const TIMER_SEC = 25 * 60;
 
