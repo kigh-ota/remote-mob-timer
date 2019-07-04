3e247905642ea79580788cccb54505156f09f264
Kaiichiro Ota <kigh.ota@gmail.com>
2019-07-04 22:12:57 +0900

    wrap with anonymous function

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,76 +1,82 @@
-window.onload = () => {
-  Notification.requestPermission();
-  setupServeEventListeners();
-  setupButtons();
-  fetch('/time')
-    .then(res => res.json())
-    .then(json => {
-      updateTime(json.time);
-    });
-};
-
-function setupServeEventListeners() {
-  const evtSource = new EventSource('/events/');
-  evtSource.addEventListener('tick', e => {
-    console.log('tick: ', e.data);
-    updateTime(parseInt(e.data));
-  });
-  evtSource.addEventListener('start', e => {
-    console.log('start: ', e.data);
-    const sec = parseInt(e.data);
-    updateTime(sec);
-    sendNotificationIfPossible(`Timer started (${secondToDisplayTime(sec)})`);
-  });
-  evtSource.addEventListener('stop', e => {
-    console.log('stop: ', e.data);
-    const sec = parseInt(e.data);
-    updateTime(sec);
-    sendNotificationIfPossible(`Timer stopped (${secondToDisplayTime(sec)})`);
-  });
-  evtSource.addEventListener('over', e => {
-    console.log('over: ', e.data);
-    sendNotificationIfPossible('Time ended');
-  });
-}
-
-function setupButtons() {
-  [25, 20, 15, 10, 5].forEach(min => {
-    document
-      .getElementsByClassName(`start-${min}-min`)[0]
-      .addEventListener('click', e => {
-        fetch(`/reset?sec=${min * 60}`, { method: 'POST' });
-      });
-  });
-  document.getElementsByClassName('toggle')[0].addEventListener('click', e => {
-    fetch(`/toggle`, { method: 'POST' })
+(() => {
+  window.onload = () => {
+    Notification.requestPermission();
+    setupServeEventListeners();
+    setupButtons();
+    fetch('/time')
       .then(res => res.json())
       .then(json => {
         updateTime(json.time);
       });
-  });
-}
+  };
 
-function updateTime(sec) {
-  document.getElementsByClassName('time')[0].textContent = secondToDisplayTime(
-    sec
-  );
-}
-
-function sendNotificationIfPossible(msg) {
-  if (Notification.permission === 'granted') {
-    const n = new Notification('Simple Timer', {
-      body: msg,
-      renotify: true,
-      tag: 'mob-timer'
+  function setupServeEventListeners() {
+    const evtSource = new EventSource('/events/');
+    evtSource.addEventListener('tick', e => {
+      console.log('tick: ', e.data);
+      updateTime(parseInt(e.data));
     });
-    n.onclick = () => window.focus();
-  }
-}
+    evtSource.addEventListener('start', e => {
+      console.log('start: ', e.data);
+      const sec = parseInt(e.data);
+      updateTime(sec);
+      sendNotificationIfPossible(`Timer started (${secondToDisplayTime(sec)})`);
+    });
+    evtSource.addEventListener('stop', e => {
+      console.log('stop: ', e.data);
+      const sec = parseInt(e.data);
+      updateTime(sec);
+      sendNotificationIfPossible(`Timer stopped (${secondToDisplayTime(sec)})`);
+    });
+    evtSource.addEventListener('over', e => {
+      console.log('over: ', e.data);
+      sendNotificationIfPossible('Time ended');
+    });
 
-function secondToDisplayTime(sec) {
-  return (
-    `${Math.floor(sec / 60)}`.padStart(2, '0') +
-    ':' +
-    `${sec % 60}`.padStart(2, '0')
-  );
-}
+    evtSource.onerror = e => console.error(e);
+  }
+
+  function setupButtons() {
+    [25, 20, 15, 10, 5].forEach(min => {
+      document
+        .getElementsByClassName(`start-${min}-min`)[0]
+        .addEventListener('click', e => {
+          fetch(`/reset?sec=${min * 60}`, { method: 'POST' });
+        });
+    });
+    document
+      .getElementsByClassName('toggle')[0]
+      .addEventListener('click', e => {
+        fetch(`/toggle`, { method: 'POST' })
+          .then(res => res.json())
+          .then(json => {
+            updateTime(json.time);
+          });
+      });
+  }
+
+  function updateTime(sec) {
+    document.getElementsByClassName(
+      'time'
+    )[0].textContent = secondToDisplayTime(sec);
+  }
+
+  function sendNotificationIfPossible(msg) {
+    if (Notification.permission === 'granted') {
+      const n = new Notification('Simple Timer', {
+        body: msg,
+        renotify: true,
+        tag: 'mob-timer'
+      });
+      n.onclick = () => window.focus();
+    }
+  }
+
+  function secondToDisplayTime(sec) {
+    return (
+      `${Math.floor(sec / 60)}`.padStart(2, '0') +
+      ':' +
+      `${sec % 60}`.padStart(2, '0')
+    );
+  }
+})();
