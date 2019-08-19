c20e833257d9d88e86c140353fbf7886535a19a7
Kaiichiro Ota <kigh.ota@gmail.com>
2019-08-19 21:59:06 +0900

    refactor app.ts

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,6 @@
 import createError from 'http-errors';
 import express, { Response } from 'express';
+import { Express } from 'express';
 import path from 'path';
 import logger from 'morgan';
 import Timer from './Timer';
@@ -10,62 +11,21 @@ import ClientInfo from '../common/ClientInfo';
 import StatusJson from '../common/StatusJson';
 import { interval } from 'rxjs';
 
-const app = express();
-const eventHistoryStore = new EventHistoryStore();
+function initializeExpress(): Express {
+  const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, '..', '..', 'views'));
-app.set('view engine', 'pug');
+  // view engine setup
+  app.set('views', path.join(__dirname, '..', '..', 'views'));
+  app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, '..', '..', 'public')));
+  app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
-const TIMER_SEC = 25 * 60;
-
-const timer = new Timer(
-  (sec: number) => sendServerEvent(EventFactory.tick(sec)),
-  () => {
-    const event = EventFactory.over();
-    sendServerEvent(event);
-    eventHistoryStore.add(event);
-  }
-);
-
-// Main Endpoint
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
-
-// Endpoint for Server-Sent Events
-// Ref. https://qiita.com/akameco/items/c54af5af35ef9b500b54
-let clientId = 0;
-let clients: { [clientId: number]: ClientInfo & { response: Response } } = {};
-app.get('/events', (req, res) => {
-  req.socket.setTimeout(43200);
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-store'
-  });
-  res.write('\n');
-  (clientId => {
-    const clientInfo = { ip: req.ip, userAgent: req.header('User-Agent') };
-    clients[clientId] = {
-      response: res,
-      ip: req.ip,
-      userAgent: req.header('User-Agent')
-    };
-    console.log(`Registered client: id=${clientId}`);
-    eventHistoryStore.add(EventFactory.clientRegistered(clientInfo));
-    req.on('close', () => {
-      delete clients[clientId];
-      console.log(`Unregistered client: id=${clientId}`);
-      eventHistoryStore.add(EventFactory.clientUnregistered(clientInfo));
-    });
-  })(++clientId);
-});
+  return app;
+}
 
 function sendServerEvent(event: IEvent) {
   const dataString = JSON.stringify(event.data);
@@ -76,18 +36,18 @@ function sendServerEvent(event: IEvent) {
   }
 }
 
-app.get('/status.json', (req, res, next) => {
-  const statusJson: StatusJson = {
-    timer: {
-      time: timer.getTime(),
-      nClient: Object.keys(clients).length,
-      isRunning: timer.isRunning()
-    },
-    clients: clientInfoMap(),
-    eventHistory: eventHistoryStore.list()
-  };
-  res.json(statusJson);
-});
+function setupTimer(eventHistoryStore: EventHistoryStore): Timer {
+  const timer = new Timer(
+    (sec: number) => sendServerEvent(EventFactory.tick(sec)),
+    () => {
+      const event = EventFactory.over();
+      sendServerEvent(event);
+      eventHistoryStore.add(event);
+    }
+  );
+  timer.setTime(TIMER_SEC);
+  return timer;
+}
 
 function clientInfoMap(): { [clientId: number]: ClientInfo } {
   const ret: { [clientId: number]: ClientInfo } = {};
@@ -100,57 +60,115 @@ function clientInfoMap(): { [clientId: number]: ClientInfo } {
   return ret;
 }
 
-app.post('/reset', (req, res, next) => {
-  timer.stop();
-  const sec = req.query.sec ? Number(req.query.sec) : TIMER_SEC;
-  timer.setTime(sec);
-  timer.start();
-  const event = EventFactory.start(sec, decodeURIComponent(req.query.name));
-  sendServerEvent(event);
-  eventHistoryStore.add(event);
-  res.send('reset');
-});
+function setupEndpoints(
+  app: Express,
+  timer: Timer,
+  eventHistoryStore: EventHistoryStore
+) {
+  // Main Endpoint
+  app.get('/', (req, res, next) => {
+    res.render('index');
+  });
 
-app.post('/toggle', (req, res, next) => {
-  if (timer.getTime() > 0) {
-    if (timer.isRunning()) {
-      timer.stop();
-      const event = EventFactory.stop(
-        timer.getTime(),
-        decodeURIComponent(req.query.name)
-      );
-      sendServerEvent(event);
-      eventHistoryStore.add(event);
-    } else {
-      timer.start();
-      const event = EventFactory.start(
-        timer.getTime(),
-        decodeURIComponent(req.query.name)
-      );
-      sendServerEvent(event);
-      eventHistoryStore.add(event);
+  // Endpoint for Server-Sent Events
+  // Ref. https://qiita.com/akameco/items/c54af5af35ef9b500b54
+  app.get('/events', (req, res) => {
+    req.socket.setTimeout(43200);
+    res.writeHead(200, {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-store'
+    });
+    res.write('\n');
+    (clientId => {
+      const clientInfo = { ip: req.ip, userAgent: req.header('User-Agent') };
+      clients[clientId] = {
+        response: res,
+        ip: req.ip,
+        userAgent: req.header('User-Agent')
+      };
+      console.log(`Registered client: id=${clientId}`);
+      eventHistoryStore.add(EventFactory.clientRegistered(clientInfo));
+      req.on('close', () => {
+        delete clients[clientId];
+        console.log(`Unregistered client: id=${clientId}`);
+        eventHistoryStore.add(EventFactory.clientUnregistered(clientInfo));
+      });
+    })(++clientId);
+  });
+
+  app.get('/status.json', (req, res, next) => {
+    const statusJson: StatusJson = {
+      timer: {
+        time: timer.getTime(),
+        nClient: Object.keys(clients).length,
+        isRunning: timer.isRunning()
+      },
+      clients: clientInfoMap(),
+      eventHistory: eventHistoryStore.list()
+    };
+    res.json(statusJson);
+  });
+
+  app.post('/reset', (req, res, next) => {
+    timer.stop();
+    const sec = req.query.sec ? Number(req.query.sec) : TIMER_SEC;
+    timer.setTime(sec);
+    timer.start();
+    const event = EventFactory.start(sec, decodeURIComponent(req.query.name));
+    sendServerEvent(event);
+    eventHistoryStore.add(event);
+    res.send('reset');
+  });
+
+  app.post('/toggle', (req, res, next) => {
+    if (timer.getTime() > 0) {
+      if (timer.isRunning()) {
+        timer.stop();
+        const event = EventFactory.stop(
+          timer.getTime(),
+          decodeURIComponent(req.query.name)
+        );
+        sendServerEvent(event);
+        eventHistoryStore.add(event);
+      } else {
+        timer.start();
+        const event = EventFactory.start(
+          timer.getTime(),
+          decodeURIComponent(req.query.name)
+        );
+        sendServerEvent(event);
+        eventHistoryStore.add(event);
+      }
     }
-  }
-  res.send({ isRunning: timer.isRunning(), time: timer.getTime() });
-});
+    res.send({ isRunning: timer.isRunning(), time: timer.getTime() });
+  });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
 
-// error handler
-app.use(<express.ErrorRequestHandler>function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(<express.ErrorRequestHandler>function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+}
 
-timer.setTime(TIMER_SEC);
+const TIMER_SEC = 25 * 60;
+const eventHistoryStore = new EventHistoryStore();
+
+let clientId = 0;
+let clients: { [clientId: number]: ClientInfo & { response: Response } } = {};
+
+const timer = setupTimer(eventHistoryStore);
+const app = initializeExpress();
+setupEndpoints(app, timer, eventHistoryStore);
 
 const SEND_ALIVE_INTERVAL_SEC = 5;
 interval(SEND_ALIVE_INTERVAL_SEC * 1000).subscribe(() =>
