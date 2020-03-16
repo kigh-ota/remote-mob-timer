4604e63d141900f56d50114fe456b10575b93fee
Kaiichiro Ota <kigh.ota@gmail.com>
2020-03-17 01:54:16 +0900

    add spotless

--- src-java/src/main/java/kigh/rmt/RemoteMobTimerApplication.java
+++ src-java/src/main/java/kigh/rmt/RemoteMobTimerApplication.java
@@ -2,15 +2,10 @@ package kigh.rmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @RestController
